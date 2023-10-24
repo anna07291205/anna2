f52492aa4b2af01de4135447bf4bd32d4052671d
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-10-24 10:35:57 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -2,3 +2,5 @@ var who66='world';
 function sayhello66(){alert('Hello, '+who66+'!');}
 function taro66(){who66='太郎';}
 function hanako66(){who66='花子';}
+function tarokita66(){who66='太郎が来た';}
+function hanakokita66(){who66='花子が来た';}

1ba36e4ff30d87a177d537b1dcf75ed0804c8b2e
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-11-07 09:46:34 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,2 +1,3 @@
 function gazo688()
-{document.getElementById('who688').innerHTML='<img src="./image/bulbon.png" alt="オン">';}
+{document.getElementById('who688').innerHTML='
+  <img src="./image/bulbon.png" alt="オン">';}

0a83d2d8230e781318acc3e9b4c00c4d99380fbf
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-11-21 09:59:40 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,2 +1,1 @@
-function hyouji(){//表示する文字 var str="一文字ずつ表示します。";
-  //テキストボックスの文字数
+function hyouji(){//表示する文字 var str="一文字ずつ表示します。";//テキストボックスの文字数 var cnt=document.timer.mojivalue.length;//文字が全部表示されているか確認

c30fd129b2fbd8b42977ca6f4bc847a42ecb7eeb
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-11-21 10:37:58 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,5 +1,12 @@
 function hyouji(){//表示する文字 
-var str="一文字ずつ表示します。";//テキストボックスの文字数 
-var cnt=document.timer.mojivalue.length;//文字が全部表示されているか確認
-if(cnt<11){//現在より１文字多く切り出して表示document.timer.moji.value=str.substr(0,cnt+1);}else{//すべて表示されたら、空文字に戻すdocument.timer.moji.value="";}}
-function startfnc(){//関数hyouji()を１０００ミリ秒単位で呼び出すsetInterval("hyouji()",1000);}
+var str="一文字ずつ表示します。";//
+  テキストボックスの文字数 
+var cnt=document.timer.mojivalue.length;
+  //文字が全部表示されているか確認
+if( cnt < 11 ){
+  //現在より１文字多く切り出して表示
+  document.timer.moji.value=str.substr(0,cnt+1);}
+else{
+  //すべて表示されたら、空文字に戻すdocument.timer.moji.value="";}}
+function startfnc(){
+  //関数hyouji()を１０００ミリ秒単位で呼び出すsetInterval("hyouji()",1000);}

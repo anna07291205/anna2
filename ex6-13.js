87bb8c3fa2392dd9551a7b0e50af12cd8ea9c610
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-11-28 10:02:42 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,1 +1,13 @@
 function mes(){alert("３秒経ちました！");}
+
+function hyouji13(){
+  //表示する文字
+var str="一文字ずつ表示します。";
+  //テキストボックスの文字数
+var cnt13=document.timer13.moji13.value.length;
+  //文字が全部表示されているか確認
+if( cnt < 11 ){
+  //現在より１文字多く切り出して表示
+  document.timer13.moji13.value=str13.substr(0,cnt13+1);}else{
+  //すべて表示されたら、空文字に戻す
+  

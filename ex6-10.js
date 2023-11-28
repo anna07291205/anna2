051d5b78a84ab52756952ec49fb2995206812469
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-11-28 10:33:03 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('logo'). src='./image/bulbon.png';}
-function imgchange1(){document.getElementById('logo'). src='./image/';}
+function imgchange2(){document.getElementById('logo'). src='./image/pngwing.com.png';}

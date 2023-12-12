14e201e9a8e865a4422f293b38da12acb45204cf
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-12-12 10:21:51 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function imgchange3(){document.getElementById('sogo'). src='./image/bulbon.png';}
+function imgchange3(){document.getElementById('sogo').src='./image/bulbon.png';}
  document.timer14.gazo14.value="";}
 setTimeout("imgchange3()",3000);}

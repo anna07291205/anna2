83576684e1f63049e23e72e4b191e23bf4c5d0cf
anna07291205 <131092063+anna07291205@users.noreply.github.com>
2023-12-19 09:30:39 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,3 +1,3 @@
-function imgchange1(){document.getElementById('logo'). src='./image/bulbon.png';}
-function imgchange2(){document.getElementById('logo'). src='./image/pngwing.com.png';}
+function imgchange5(){document.getElementById('dogo'). src='./image/bulbon.png';}
+function imgchange6(){document.getElementById('dogo'). src='./image/pngwing.com.png';}
 
